bfd8674f7b7cf2cd9d9febf35440773d54ccfccf
Stuart <batecsw@hotmail.com>
2019-06-25 09:24:14 +0200

    Init commit repo

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MYyroot exercises folder</title>
+</head>
+<body>
+<?php
+    echo '<h1>This is my parent exercises folder<h1>'
+?>
+    
+</body>
+</html>
